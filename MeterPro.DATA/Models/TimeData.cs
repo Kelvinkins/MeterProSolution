d7242de3818b93884551f224f90a2ca36d28c6fb
Kingsley <kins4swagg@gmail.com>
2023-10-13 10:31:55 +0100

    Added name jsonPropertyName attribute to the properties

--- MeterPro.DATA/Models/TimeData.cs
+++ MeterPro.DATA/Models/TimeData.cs
@@ -7,127 +7,192 @@ using System.Threading.Tasks;
 
 namespace MeterPro.DATA.Models
 {
+
+    using System.Text.Json.Serialization;
+
     public class TimeData
     {
-
+        [JsonPropertyName("TempC")]
         public string? TempC { get; set; }
+
+        [JsonPropertyName("TempB")]
         public string? TempB { get; set; }
+
+        [JsonPropertyName("TempA")]
         public string? TempA { get; set; }
+
+        [JsonPropertyName("PT")]
         public string? PT { get; set; }
+
+        [JsonPropertyName("DO2")]
         public string? DO2 { get; set; }
+
+        [JsonPropertyName("D01")]
         public string? D01 { get; set; }
+
+        [JsonPropertyName("source")]
         public string? source { get; set; }
+
+        [JsonPropertyName("UpInterval")]
         public string? UpInterval { get; set; }
+
+        [JsonPropertyName("Pa")]
         public string? Pa { get; set; }
+
+        [JsonPropertyName("Pb")]
         public string? Pb { get; set; }
+
+        [JsonPropertyName("Pc")]
         public string? Pc { get; set; }
 
-        //        "EQC": "12.84",
-        //"id": "1132",
-        //"Lg": "0",
-        //"state": "ONLINE",
-        //"EQL": "0",
-        //"IN": "1.98",
-        //"EPID": "0.392",
-        //"EPIF": "0",
-        //"EPIG": "0",
-        //"PFa": "0.951",
-        //"EP": "36.34",
+        [JsonPropertyName("EQC")]
         public string? EQC { get; set; }
+
+        [JsonPropertyName("id")]
         public string? id { get; set; }
+
+        [JsonPropertyName("Lg")]
         public string? Lg { get; set; }
+
+        [JsonPropertyName("state")]
         public string? state { get; set; }
+
+        [JsonPropertyName("EQL")]
         public string? EQL { get; set; }
+
+        [JsonPropertyName("IN")]
         public string? IN { get; set; }
+
+        [JsonPropertyName("EPID")]
         public string? EPID { get; set; }
+
+        [JsonPropertyName("EPIF")]
         public string? EPIF { get; set; }
+
+        [JsonPropertyName("EPIG")]
         public string? EPIG { get; set; }
+
+        [JsonPropertyName("PFa")]
         public string? PFa { get; set; }
+
+        [JsonPropertyName("EP")]
         public string? EP { get; set; }
 
-
-        //"Ua": "235",
-        //"PFc": "1",
-        //"EPIJ": "0",
-        //"Ub": "235.9",
-        //"PFb": "1",
-        //"Uc": "235.8",
-        //"Qa": "-0.128",
-        //"Qb": "0",
-        //"Qc": "0",
-        //"EPIP": "0",
-        //"meterSn": "12212122890001",
-        //"Uab": "407.8",
-        //"Ia": "1.98",
+        [JsonPropertyName("Ua")]
         public string? Ua { get; set; }
+
+        [JsonPropertyName("PFc")]
         public string? PFc { get; set; }
+
+        [JsonPropertyName("EPIJ")]
         public string? EPIJ { get; set; }
+
+        [JsonPropertyName("Ub")]
         public string? Ub { get; set; }
+
+        [JsonPropertyName("PFb")]
         public string? PFb { get; set; }
+
+        [JsonPropertyName("Uc")]
         public string? Uc { get; set; }
+
+        [JsonPropertyName("Qa")]
         public string? Qa { get; set; }
+
+        [JsonPropertyName("Qb")]
         public string? Qb { get; set; }
+
+        [JsonPropertyName("Qc")]
         public string? Qc { get; set; }
+
+        [JsonPropertyName("EPIP")]
         public string? EPIP { get; set; }
+
+        [JsonPropertyName("meterSn")]
         public string? meterSn { get; set; }
+
+        [JsonPropertyName("Ia")]
         public string? Ia { get; set; }
-        //        "Ib": "0",
-        //"Ic": "0",
-        //"MEPIMD": "0.407",
-        //"timezone": "8",
-        //"DI0": "0",
-        //"DI2": "0",
-        //"DI1": "0",
-        //"DI3": "0",
-        //"P": "0.394",
+
+        [JsonPropertyName("Ib")]
         public string? Ib { get; set; }
+
+        [JsonPropertyName("Ic")]
         public string? Ic { get; set; }
+
+        [JsonPropertyName("MEPIMD")]
         public string? MEPIMD { get; set; }
+
+        [JsonPropertyName("timezone")]
         public string? timezone { get; set; }
+
+        [JsonPropertyName("DI0")]
         public string? DI0 { get; set; }
+
+        [JsonPropertyName("DI2")]
         public string? DI2 { get; set; }
+
+        [JsonPropertyName("DI1")]
         public string? DI1 { get; set; }
+
+        [JsonPropertyName("DI3")]
         public string? DI3 { get; set; }
+
+        [JsonPropertyName("P")]
         public string? P { get; set; }
 
-        //"Q": "-0.13",
-        //"S": "0.416",
-        //"Ubc": "408.5",
-        //"meterNo": "12212122890001_12212122890001",
-        //"timestamp": 1672021200000,
-        //"CreateTime": "2022-12-26 10:20:00",
-        //"msgid": 767178432396455936,
-        //"MEPIMDT": "2022-12-23 12:50",
-        //"Fr": "50.02",
-        //"Sa": "0.414",
-        //"Sb": "0",
-
+        [JsonPropertyName("Q")]
         public string? Q { get; set; }
+
+        [JsonPropertyName("S")]
         public string? S { get; set; }
+
+        [JsonPropertyName("Ubc")]
         public string? Ubc { get; set; }
+
+        [JsonPropertyName("meterNo")]
         public string? meterNo { get; set; }
+
+        [JsonPropertyName("timestamp")]
         public string? timestamp { get; set; }
+
+        [JsonPropertyName("CreateTime")]
         public string? CreateTime { get; set; }
+
+        [JsonPropertyName("msgid")]
         public string? msgid { get; set; }
+
+        [JsonPropertyName("MEPIMDT")]
         public string? MEPIMDT { get; set; }
+
+        [JsonPropertyName("Fr")]
         public string? Fr { get; set; }
+
+        [JsonPropertyName("Sa")]
         public string? Sa { get; set; }
+
+        [JsonPropertyName("Sb")]
         public string? Sb { get; set; }
-        //        "Sc": "0",
-        //"CT": "1",
-        //"Uca": "407.7",
-        //"PF": "0.949",
-        //"EPE": "0",
-        //"gatewaySn": "12212122890001",
-        //"EPI": "36.34
+
+        [JsonPropertyName("Sc")]
         public string? Sc { get; set; }
+
+        [JsonPropertyName("CT")]
         public string? CT { get; set; }
+
+        [JsonPropertyName("Uca")]
         public string? Uca { get; set; }
+
+        [JsonPropertyName("PF")]
         public string? PF { get; set; }
+
+        [JsonPropertyName("EPE")]
         public string? EPE { get; set; }
+
+        [JsonPropertyName("gatewaySn")]
         public string? gatewaySn { get; set; }
+
+        [JsonPropertyName("EPI")]
         public string? EPI { get; set; }
-
-
     }
 }
