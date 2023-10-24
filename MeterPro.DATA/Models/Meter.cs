8186d6d40e446198056f1cc1fa54f86b3df60a75
Kingsley <kins4swagg@gmail.com>
2023-10-24 01:28:53 +0100

    Added Meters Collection

---
+++ MeterPro.DATA/Models/Meter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeterPro.DATA.Models
+{
+    public class Meter
+    {
+        public string? MeterSn { get; set; }
+        public string? gatewaySn { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string? Status { get; set; }
+        public DateTime? DateEnrolled { get; set; }
+        public DateTime? LastUpdated { get; set; }
+        public string? PowerStatus { get; set; }
+
+
+    }
+}
