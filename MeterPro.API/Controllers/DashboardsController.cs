0063d8494918052d29fb757112278af74cdd58c0
Kingsley <kins4swagg@gmail.com>
2023-10-27 20:50:59 +0100

    Added  new endpoints

---
+++ MeterPro.API/Controllers/DashboardsController.cs
@@ -0,0 +1,39 @@
+﻿using MeterPro.DATA.DAL;
+using MeterPro.DATA.Models;
+using MeterPro.DATA.StatisticModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace MeterPro.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardsController : ControllerBase
+    {
+
+        private readonly UnitOfWork unitOfWork;
+        public DashboardsController(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDashboard(string meterSn)
+        {
+            var filter = Builders<Meter>.Filter;
+            var query = filter.Empty;
+            var meters = await unitOfWork.MeterDataRepository.GetAll(query);
+            var dash = new DashData()
+            {
+                AllMetersCount = meters.Count(),
+                ConnectedMetersCount = meters.Where(a => a.Status == "ONLINE").Count(),
+                DisconnectedMetersCount = meters.Where(a => a.Status == "OFFLINE").Count(),
+                PowerOutMeters = meters.Where(a => a.PowerStatus == "OFF").Count()
+
+            };
+            return Ok(dash);
+        }
+
+    }
+}
