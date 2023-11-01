bf0020b9b39b840420c6b3e6299e4f0b27005144
Kingsley <kins4swagg@gmail.com>
2023-11-01 18:52:54 +0100

    Device status logic2

---
+++ MeterPro.DATA/Utils/DateTimeHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeterPro.DATA.Utils
+{
+    public static class DateTimeHelper
+    {
+        public static bool HasNotReportedInLastTwoMinutes(DateTime lastUpdated)
+        {
+            DateTime currentTime = DateTime.Now;
+            TimeSpan timeDifference = currentTime - lastUpdated;
+            if (timeDifference.TotalMinutes > 2)
+            {
+                return true; // Device has not reported in the last 10 minutes
+            }
+
+            return false; // Device has reported in the last 10 minutes
+        }
+    }
+}
